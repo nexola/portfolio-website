e133ef672c5255bb16d77aed9495499ade2cf0ac
Vitor Vianna <vitormatheusfv@gmail.com>
2023-09-14 17:32:10 -0300

    Animacao ao scroll

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ if (linksInternos.length) {
     link.addEventListener("click", scrollToSection);
   });
 }
+
+function animacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((elemento) => {
+        const sectionTop = elemento.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          elemento.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+animacaoScroll();
